4421b95d31954fb2b7395d2b00de560da73c4408
slsl2 <kimminji7153@gmail.com>
2024-06-27 16:05:29 +0900

    add: api 불러온 데이터 타입 설정

---
+++ src/api/countries.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { Country } from "../types/country";
+
+export const getCountries = async (): Promise<Country[]> => {
+  try {
+    const response = await axios.get("https://restcountries.com/v3.1/all");
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
